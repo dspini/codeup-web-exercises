0b3cf6498cb90ea6a8c9d5d0a67c2a28251b8183
Donald Spini <donald.spini68@gmail.com>
2020-07-07 09:15:56 -0500

    morning warmups

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,7 +1,8 @@
 (function () {
     "use strict";
 
-    let fizzbuzz = function (num) {
+    console.log("fizzbuz1");
+    let fizzbuzz1 = function (num) {
         for (let i = 1; i <= num; i++) {
             if (i % 3 === 0 && i % 5 === 0) {
                 console.log("fizzbuzz");
@@ -14,6 +15,55 @@
             }
         }
     }
-    fizzbuzz( 31);
+    fizzbuzz1( 31);
 
+    console.log("fizzbuzz2");
+    let fizzbuzz2 = function (num) {
+        for (let i = 1; i <= num; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+            } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+               console.log(i);
+            }
+        }
+    }
+    fizzbuzz2(31);
+
+    console.log("fizzbuzz3");
+    let fizzbuzz3 = function(num) {
+        for (let i = 1; i <= num; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+            } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+            console.log(i);
+            }
+        }
+    }
+    fizzbuzz3(31);
+
+    console.log("fizzbuzz4");
+    let fizzbuzz4 = function(num) {
+        for (let i = 1; i <= num; i ++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+            } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
+    }
+    fizzbuzz4(31)
+
+    //document.getElementById("demo")//.innerHTML = htmlString;
 })();
