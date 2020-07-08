0f752ff9c5c13e10e7b864dd523c469159461575
Donald Spini <donald.spini68@gmail.com>
2020-07-08 08:20:40 -0500

    fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -65,5 +65,39 @@
     }
     fizzbuzz4(31)
 
+    console.log("fizzbuzz5");
+    let fizzbuzz5 = function(num) {
+        for (let i = 1; i <= num; i++) {
+            if (i % 5 === 0 && i % 3 === 0) {
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+        ``  } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
+    }
+    fizzbuzz5(31);
+
+    console.log("fizzbuzz6");
+    let fizzbuzz6 = function(num) {
+        for (let i = 1; i <= num; i++) {
+            if (i % 5 === 0 && i % 3 === 0) {
+                console.log("fizzbuzz");
+            } else if (i % 3 === 0) {
+                console.log("fizz");
+            } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
+    }
+    fizzbuzz6(31)
+
+
+
     //document.getElementById("demo")//.innerHTML = htmlString;
 })();
