76fa931e223be0e6a8300e087296b73cebdd2414
Donald Spini <donald.spini68@gmail.com>
2020-07-27 16:24:20 -0500

    weathermap

--- js/weathermap.js
+++ js/weathermap.js
@@ -32,61 +32,26 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
         // Pressure
         let pressure = data.daily[i].pressure;
         $(cardId + " #pressure").html(pressure + " mBAR");
-        console.log(data.daily[i].pressure)
+        console.log(data.daily[i].pressure);
+        // Icons
+        let icon = data.daily[i].id;
+        $(cardId + "#icon").html(icon);
+        console.log(data.daily[i].id);
     }
 });
 
-/***
- * geocode is a method to search for coordinates based on a physical address and return
- * @param {string} search is the address to search for the geocoded coordinates
- * @param {string} token is your API token for MapBox
- * @returns {Promise} a promise containing the latitude and longitude as a two element array
- *
- * EXAMPLE:
- *
- *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
- *      // do something with results
- *  })
- *
- */
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-            // to get all the data from the request, comment out the following three lines...
-        }).then(function(data) {
-            return data.features[0].center;
-        });
-}
 
-/***
- * reverseGeocode is a method to search for a physical address based on inputted coordinates
- * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
- * @param {string} token is your API token for MapBox
- * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
- *
- * EXAMPLE:
- *
- *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
- *      // do something with results
- *  })
- *
- */
-function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-        })
-        // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
-}
+var accessToken = mapboxKey;
+mapboxgl.accessToken = accessToken;
 
+mapboxgl.accessToken = mapboxKey;
+let map = new mapboxgl.Map({
+    container: "mapboxgl-canvas-container",
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 12,
+    center: [-98.4936282, 29.4241219]
+});
 
+map.addControl(new mapboxgl.NavigationControl());
 
 
