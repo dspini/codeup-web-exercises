217a664d0fe3da969ed19f18bb2400e4d845bfa3
Donald Spini <donald.spini68@gmail.com>
2020-07-30 18:45:40 -0500

    weathermap

--- js/weather_map.js
+++ js/weather_map.js
@@ -15,29 +15,37 @@ $(document).ready(function() {
             let cardId = "#card" + i
             // Date
             let date = new Date(data.daily[i].dt * 1000);
-            $(cardId + " #weatherDate").html(date.toDateString());
-            console.log(data.daily[i].dt)
+                $(cardId + " #weatherDate").html(date.toDateString());
+                    console.log(data.daily[i].dt)
             // Temp
             let lowTemp = data.daily[i].temp.min;
             let highTemp = data.daily[i].temp.max;
-            $(cardId + " #highLowTemp").html(lowTemp + "°F / " + highTemp + "°F");
-            console.log(data.daily[i].temp)
+                $(cardId + " #highLowTemp").html(lowTemp + "°F / " + highTemp + "°F");
+                    console.log(data.daily[i].temp)
             // Feels Like
             let feelsLike = data.daily[i].feels_like.day;
-            $(cardId + " #feelsLike").html(feelsLike + "%");
-            console.log(data.daily[i].feels_like)
+                $(cardId + " #feelsLike").html(feelsLike + "%");
+                    console.log(data.daily[i].feels_like)
             // Humidity
             let humidity = data.daily[i].humidity;
-            $(cardId + " #humidity").html(humidity + "°F");
-            console.log(data.daily[i].humidity)
+                $(cardId + " #humidity").html(humidity + "°F");
+                    console.log(data.daily[i].humidity)
             // Wind
             let wind = data.daily[i].wind_speed;
-            $(cardId + " #wind").html(wind + " MPH");
-            console.log(data.daily[i].wind_speed)
+                $(cardId + " #wind").html(wind + " MPH");
+                    console.log(data.daily[i].wind_speed)
             // Pressure
             let pressure = data.daily[i].pressure;
-            $(cardId + " #pressure").html(pressure + " mBAR");
-            console.log(data.daily[i].pressure);
+                $(cardId + " #pressure").html(pressure + " mBAR");
+                    console.log(data.daily[i].pressure);
+            // Icons
+            let icon = `http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@4x.png`;
+            for (var i = 0; i < 5; i++) {
+            let cardId = "#card" + i
+                icon = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@4x.png`;
+                $(cardId + " #icon img").attr("src", icon);
+                    console.log(data.daily[i].weather.icon);
+            }
         }
     });
 
@@ -46,12 +54,12 @@ $(document).ready(function() {
     var map = new mapboxgl.Map({
         container: 'map', // Container ID
         style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-        center: [-122.25948, 37.87221], // Starting position [lng, lat]
+        center: [-98.4936282, 29.4241219], // Starting position [lng, lat]
         zoom: 12, // Starting zoom level
     });
 
     var marker = new mapboxgl.Marker() // Initialize a new marker
-        .setLngLat([-122.25948, 37.87221]) // Marker [lng, lat] coordinates
+        .setLngLat([-98.4936282, 29.4241219]) // Marker [lng, lat] coordinates
         .addTo(map); // Add the marker to the map
 
     var geocoder = new MapboxGeocoder({ // Initialize the geocoder
@@ -59,11 +67,7 @@ $(document).ready(function() {
         mapboxgl: mapboxgl, // Set the mapbox-gl instance
         marker: false, // Do not use the default marker style
         placeholder: 'Search for places in Berkeley', // Placeholder text for the search bar
-        bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
-        proximity: {
-            longitude: -122.25948,
-            latitude: 37.87221
-        } // Coordinates of UC Berkeley
+        flyTo: true
     });
 
     // Add the geocoder to the map
@@ -77,48 +81,26 @@ $(document).ready(function() {
             data: {
                 type: 'FeatureCollection',
                 features: []
-            }
-        });
+        }
+    });
 
-        map.addLayer({
-            id: 'point',
-            source: 'single-point',
-            type: 'circle',
-            paint: {
-                'circle-radius': 10,
-                'circle-color': '#448ee4'
-            }
-        });
+    map.addLayer({
+        id: 'point',
+        source: 'single-point',
+        type: 'circle',
+        paint: {
+            'circle-radius': 10,
+            'circle-color': '#448ee4'
+        }
+    });
 
-        // Listen for the `result` event from the Geocoder
-        // `result` event is triggered when a user makes a selection
-        // Add a marker at the result's coordinates
-        geocoder.on('result', function(ev) {
-            map.getSource('single-point').setData(ev.result.geometry);
+    // Listen for the `result` event from the Geocoder
+    // `result` event is triggered when a user makes a selection
+    // Add a marker at the result's coordinates
+    geocoder.on('results', function(ev) {
+    // map.getSource('single-point').setData(ev.result.geometry);
+        console.log(ev);
         });
     });
-    // Mapbox API Controls
-    //map.addControl(new mapboxgl.NavigationControl());
-
-    // Weathermap API Icons JS
-    // let icon = "http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@4x.png";
-    // for (var i = 0; i < 5; i++) {
-    //     let cardId = "#card" + i
-    //     icon = "http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@4x.png";
-    //     $(cardId + " #icon").html(icon);
-    //     console.log(data.daily[i].weather.icon);
-    // }
-
-    // Navigation Search Input JS
-    // function geocode(search, token) {
-    //     var baseUrl = 'https://api.mapbox.com';
-    //     var endPoint = '/geocoding/v5/mapbox.places/';
-    //     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-    //         .then(function(res) {
-    //             return res.json();
-    //         }).then(function(data) {
-    //             return data.features[0].center;
-    //         });
-    // }
 
 }); // close tag
